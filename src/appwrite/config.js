5546cf2901c80598720731a72b36dc6e9c3c25d1
Simran-Dhyani <dhyanisimran7@gmail.com>
2026-02-03 21:06:27 +0530

    Initial commit - React + Appwrite blog project

---
+++ src/appwrite/config.js
@@ -0,0 +1,129 @@
+import conf from "../conf/conf";
+import { Client, ID,Databases,Storage,Query} from "appwrite";
+export class Service{ 
+    client=new Client();
+    databases;
+    bucket;
+
+    constructor(){
+         this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+          this.databases=new TablesDBs(this.client);
+          this.bucket=new Storage(this.client);
+    }
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+            await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteTableId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteTableId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+
+            )
+        } catch (error) {
+            throw error;
+            
+        }
+
+    }
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteTableId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            throw error;
+        
+        }
+
+    }
+
+    async getPost(slug){
+        try {
+           return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteTableId,
+                slug
+            )
+        } catch (error) {
+            throw error;
+        }
+
+    }
+    async getPosts(queries=[Query.equal("status","active")]){
+        try {
+            await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteTableId,
+                queries,
+            
+
+            )
+        } catch (error) {
+            throw error;
+    
+        }
+
+    }
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+
+            );
+        } catch (error) {
+           throw error;
+            
+        }
+    }
+    async deleteFile(fileID){
+        try {
+             await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileID
+            )
+            return true;
+        } catch (error) {
+            throw error;
+            
+        }
+    }
+     getFileDownload(fileID){
+        return this.bucket.getFileDownload(
+            conf.appwriteBucketId,
+            fileID
+        )
+
+     }
+}
+const service=new Service();
+export default service;
