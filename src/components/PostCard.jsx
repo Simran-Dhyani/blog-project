63fff957058acfc894f80b5b35f049b496ed78e2
Simran-Dhyani <dhyanisimran7@gmail.com>
2026-02-07 23:41:14 +0530

    login added

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import service from "../appwrite/config";
+import { Link } from "react-router-dom";
+function PostCard({
+    $id,title,featuredImage
+}){
+    return(
+        <Link to={`/post/${$id}`}>
+            <div className="bg-gray-100 w-full rounded-lg p-4">
+                <div className="w-full justify-center mb-4">
+                    <img src={service.getFileDownload(featuredImage)} alt={title}
+                    className="rounded-xl"
+                    ></img>
+
+                </div>
+                <h2>{title}</h2>
+
+            </div>
+        </Link>
+    )}
+    export default PostCard;
+
+
+                
--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,15 @@ import Footer from "./footer/footer";
 import container from "./container/container";
 import Logo from "./Logo";
 import LogOutBtn from "./Header/logOutBtn";
+import Button from "./Button";
+import Input from "./Input";
+
 export{
     Header,
     Footer,
     container,
     Logo,
-    LogOutBtn
+    LogOutBtn,
+    Button,
+    Input
 }
